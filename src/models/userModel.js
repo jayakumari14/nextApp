d72015fb5dea0446e316203d62553572b3c3e862
jayak <kumarijaya127@gmail.com>
2024-07-01 13:23:18 +0530

    usermodel created

---
+++ src/models/userModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+});
